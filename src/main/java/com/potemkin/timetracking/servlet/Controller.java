f6a942d26f864bc0856a625d36209f5beebaf46a
Alex <78691297+al-potemkin@users.noreply.github.com>
2021-03-04 23:12:54 +0200

    Update Controller.java

--- src/main/java/com/potemkin/timetracking/servlet/Controller.java
+++ src/main/java/com/potemkin/timetracking/servlet/Controller.java
@@ -41,7 +41,6 @@ public class Controller extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException      if an I/O error occurs
      * <p>
-     * Created by Yaroslav Bodyak on 11.12.2018.
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
